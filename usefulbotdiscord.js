0439e667e0db8929571f955a57e218194ee4db2e
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-08-06 14:14:34 +0200

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -1,29 +1,9 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+Bot.on('ready', function() {
+    console.log("Je suis prêt!");
+    Bot.user.setActivity('En phase de test.', { type: 'WATCHING' })
 });
-
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
