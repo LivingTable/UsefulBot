a2b235f1e13c3eebeea11c96cfb7892c770e6f84
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-08-06 20:20:10 +0200

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -85,7 +85,7 @@ client.on('message', message => {
       if(command === "chat") {
       // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
       // To get the "message" itself we join the `args` back into a string with spaces: 
-      const sayMessage = args.join(" ");
+      const sayMessage = args
           
       message.delete().catch(O_o=>{});
       // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
