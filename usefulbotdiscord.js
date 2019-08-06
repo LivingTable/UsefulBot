0c19fdc6080fc265d68045c85133410bc40bdbb5
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-08-06 21:21:14 +0200

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -78,16 +78,19 @@ client.on("message", (message) => {
 });
 
 //Global Chat
-
 client.on('message', message => {
-  if (message.content.startsWith("/chat "))
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+      if(command === "chat") {
+      // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+      // To get the "message" itself we join the `args` back into a string with spaces: 
+      const sayMessage = args.join(" ");
           
       message.delete().catch(O_o=>{});
       //L'envoi du message (ajoutez une ligne en changeant l'id pour ajouter un autre salon à cibler)
       const args = message.content.slice(prefix.length).trim().split(/ +/g);
-      client.channels.get(`608355997387456513`).send(args)
+      client.channels.get(`608355997387456513`).send(sayMessage)
   
- } else
   }); 
 
 client.login(process.env.BOT_TOKEN);//Héhé
