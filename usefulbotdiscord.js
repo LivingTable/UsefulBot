96b6244a59fbaf11aea24668cf65be9ba51ae403
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-08-08 05:54:09 +0200

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -150,7 +150,8 @@ client.on('message', message => {
      message.channel.send(usefulEmbed).then(sentEmbed => {
      sentEmbed.react("608484799505760272")
      sentEmbed.react("608484799216222218")
-     })     
+     }) 
+  }
 }); 
 
 client.on("serverNewMember", (server, user) => {
