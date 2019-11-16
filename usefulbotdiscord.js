021b52986fce3107cc2ad17b19509c3b528f4f1b
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-11-16 13:56:22 +0100

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
 var bossla = 0;
-var money = require('discord-money');
 var bossvie = 1000;
 const prefix = "/";
 const EventEmitter = require('events');
