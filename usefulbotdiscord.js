d462c08d023b530787599009a12818faf0a13057
noahD0803 <39674338+noahD0803@users.noreply.github.com>
2019-11-27 19:21:19 +0100

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -103,7 +103,7 @@ client.on("message", (message) => {
 client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-      if(command === "TALK") {
+      if(command === "/dis") {
       // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
       // To get the "message" itself we join the `args` back into a string with spaces: 
       const sayMessage = args.join(" ");
