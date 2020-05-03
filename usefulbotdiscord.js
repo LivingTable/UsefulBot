98c5f221e20b598d0227bc8e560865f44978c220
LivingTable <39674338+LivingTable@users.noreply.github.com>
2020-05-03 18:36:23 +0200

    Update usefulbotdiscord.js

--- usefulbotdiscord.js
+++ usefulbotdiscord.js
@@ -321,7 +321,7 @@ if (message.author.id == 376387562161438730) {
 if (message.content.includes('/nickname')) {
 	let cmember = message.mentions.members.first();
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.cmember.setNickname(message.content.replace('/nick <@${cmember.id}>', ''));
+    cmember.user.setNickname(message.content.replace('/nick <@${cmember.id}>', ''));
         }
   }
 });
